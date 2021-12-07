c4f9181fca338570392fdcad7ce1361ece64e1ad
mariotinelli <49689215+mariotinelli@users.noreply.github.com>
2021-12-07 04:19:51 -0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="/img/2021 - Teste PHP - BTZ Transportes.png" alt="BTZ Transports"> 
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
